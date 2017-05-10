92cddcf83cdd927fef478ced7902b893a0da4d27
Peter Kang <peter.kang@cmlmediasoft.local>
2017-05-10 13:26:30 -0700

    auth tk

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,9 +7,39 @@ export const addLocation = (location) => ({
 	payload: location
 })
 
+const headers = {
+	// 'Token YOUR_API_TOKEN_KEY'
+	'Authorization': 'Token giwaWhXqCRjlPltTiV0V8sbz_PMo1FecdpBbmiVy',
+	'Content-Type': 'application/json',
+	'Accept_Language': 'en_EN'
+}
+
 export const findProducts = (name) => {
-	return ({
-		type: FIND_PRODUCTS,
-		payload: name
-	})
+	return (dispatch, getState) => {
+		
+		const { startLatitude, startLongitude, endLatitude, endLongitude } = getState().location;
+
+		// arguments:
+		// 1. is the api that we will hit = uber.com/startlocation,endlocation
+		// 2. method, GET, POST
+			// metadata is data that describes data aka headers
+			//  and provides authentications
+				// pending, full-filled, or rejected
+
+		// `` = backticks
+		// this fetch returns a promise
+		fetch(`https://api.uber.com/v1.2/estimates/price?start_latitude=${startLatitude}&start_longitude=${startLongitude}&end_latitude=${endLatitude}&end_longitude=${endLongitude}`,{
+			method: 'GET',
+			headers, // same thing as 'headers: headers'
+		})
+		.then((data) => data.json())
+		.then((estimationData) => {
+			console.log('estimationData: ', estimationData);
+		})
+	}
+
+	// return ({
+	// 	type: FIND_PRODUCTS,
+	// 	payload: name
+	// })
 }
